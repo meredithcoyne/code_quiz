45bd01f38e57edc9075dce082a35b730c948b939
Meredith Coyne <meredithleigh.coyne@gmail.com>
2021-09-28 18:48:34 -0400

    updated css to add additional visuals, highscore HTML for an additional vissal id, and highscores js.

--- Assets/js/highscores.js
+++ Assets/js/highscores.js
@@ -3,12 +3,7 @@ var highScore = document.querySelector("#highScore");
 var clear = document.querySelector("#clear");
 var goBack = document.querySelector("#goBack");
 
-// Event listener to clear scores 
-clear.addEventListener("click", function () {
-    localStorage.clear();
-    location.reload();
-});
-// Retrieves local storage 
+
 var allScores = localStorage.getItem("allScores");
 allScores = JSON.parse(allScores);
 
