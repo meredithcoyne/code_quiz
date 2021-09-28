d19dad82261e98475ddf81389ae0038405ec0c81
Meredith Coyne <meredithleigh.coyne@gmail.com>
2021-09-28 19:14:08 -0400

    fixed error in clearing highscores and added a video to README file.

--- Assets/js/highscores.js
+++ Assets/js/highscores.js
@@ -3,6 +3,11 @@ var highScore = document.querySelector("#highScore");
 var clear = document.querySelector("#clear");
 var goBack = document.querySelector("#goBack");
 
+// Event listener to clear scores 
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
 
 var allScores = localStorage.getItem("allScores");
 allScores = JSON.parse(allScores);
